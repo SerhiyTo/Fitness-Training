a814bd978f7d96931823839e654548a84283fd6f
Serhii Tolmachov <cvaizencva@gmail.com>
2024-03-26 02:36:59 +0200

    Make diet app + add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.11.4
+
+# Create and set the working directory
+WORKDIR /usr/src/app
+
+# Copy project files into the container
+COPY pyproject.toml poetry.lock /usr/src/app/
+
+# Install Poetry
+RUN pip install poetry
+
+# Install project dependencies
+RUN poetry install
+
+# Copy the project code into the container
+COPY . .
--- api/training/admin.py
+++ api/training/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
-from .models import TrainingPlan, Training, FitnessExercise
+from api.training.models import TrainingPlan, Training, FitnessExercise
 
 
 @admin.register(TrainingPlan)
 class TrainingPlanAdmin(admin.ModelAdmin):
-    list_display = ("user", "coach")
+    list_display = ["user", "coach"]
 
 
 @admin.register(Training)
 class TrainingAdmin(admin.ModelAdmin):
-    list_display = ("training_plan", "fitness_exercise", "days_of_week")
+    list_display = ["training_plan", "fitness_exercise", "days_of_week"]
 
 
 @admin.register(FitnessExercise)
 class FitnessExerciseAdmin(admin.ModelAdmin):
-    list_display = (
+    list_display = [
         "name",
         "description",
         "video_link",
@@ -26,4 +26,4 @@ class FitnessExerciseAdmin(admin.ModelAdmin):
         "duration",
         "calories",
         "rating",
-    )
+    ]
