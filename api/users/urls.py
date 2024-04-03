5bcbac8140a567596a48219936e535e588e3a092
Serhii Tolmachov <cvaizencva@gmail.com>
2024-04-03 03:48:45 +0300

    Update views` structures

--- api/users/urls.py
+++ api/users/urls.py
@@ -4,5 +4,5 @@ from api.users.views import BaserProfileObtainToken, UserRegisterView
 
 urlpatterns = [
     path("token/", BaserProfileObtainToken.as_view(), name="user_login"),
-    path("register/<str:profile_type>/", UserRegisterView.as_view(), name="user_registration"),
+    path("register/", UserRegisterView.as_view(), name="user_registration"),
 ]
