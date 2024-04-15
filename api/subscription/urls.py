19603bde2fa14a73464979abe04e94c8118725c8
Serhii Tolmachov <cvaizencva@gmail.com>
2024-04-15 21:42:20 +0300

    Update urls for subscriptions view

--- api/subscription/urls.py
+++ api/subscription/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from api.subscription.views import SubscriptionView
 
 urlpatterns = [
-    path("subscriptions/", SubscriptionView.as_view(), name="subscriptions"),
+    path("", SubscriptionView.as_view(), name="subscriptions"),
     path("subscriptions/<int:pk>/", SubscriptionView.as_view(), name="subscription-detail"),
 ]
