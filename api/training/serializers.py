4dbb11c6a20f0b56c5ccad0e4f6c7c359637263b
Serhii Tolmachov <cvaizencva@gmail.com>
2024-04-17 20:26:17 +0300

    Update serializers for training

---
+++ api/training/serializers.py
@@ -0,0 +1,56 @@
+from rest_framework import serializers
+
+from api.training.models import TrainingPlan, Training, FitnessExercise
+from api.users.models import CoachProfile, UserProfile
+
+
+class TrainingPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TrainingPlan
+        fields = [
+            "user",
+            "coach",
+        ]
+
+    def validate(self, attrs):
+        if not UserProfile.objects.filter(id=attrs["user"]).exists():
+            raise serializers.ValidationError({"user": "User does not exist"})
+        if not CoachProfile.objects.filter(id=attrs["coach"]).exists():
+            raise serializers.ValidationError({"coach": "Coach does not exist"})
+        return attrs
+
+
+class TrainingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Training
+        fields = [
+            "training_plan",
+            "fitness_exercise",
+            "days_of_week",
+        ]
+
+    def validate(self, attrs):
+        if not TrainingPlan.objects.filter(id=attrs["training_plan"]).exists():
+            raise serializers.ValidationError({"training_plan": "Training plan does not exist"})
+        return attrs
+
+
+class FitnessExerciseSerializer(serializers.ModelSerializer):
+    training = TrainingSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = FitnessExercise
+        fields = [
+            "name",
+            "description",
+            "video_link",
+            "image_link",
+            "exercise_type",
+            "difficulty",
+            "muscle_group",
+            "equipment",
+            "duration",
+            "calories",
+            "rating",
+            "training",
+        ]
